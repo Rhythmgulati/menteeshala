a47374ab2f0ae50fdab2e25584276f402627ccbf
rhythm gulati <rhythmgulati11@gmail.com>
2024-04-02 13:47:32 +0530

    login routes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require("path");
 require("./db/conn")
 const PORT = process.env.PORT || 3000;
 const bodyparsser= require("body-parser");
+const bcrypt = require("bcrypt");
+
 app.use(bodyparsser.urlencoded({extended:true}));
 const hbs = require("hbs");
 app.set("view engine","hbs");
@@ -21,37 +23,74 @@ app.get("/login",(req,res)=>{
 });
 app.get("/register",(req,res)=>{
     res.render("reg");
-})
+});
 app.get("/mentorregister",(req,res)=>{
     res.render("regmentor");
-})
+});
 
-app.post("/register",(req,res)=>{
+// register
+
+
+app.post("/register",async (req,res)=>{
     const {fname,lname,email,password,study_level,course} = req.body;
     console.log(req.body);
+    const hashedpassword =  await bcrypt.hash(password,10);
     const newmentee = new Mentee({
         fname,
         lname,
         email,
-        password,
+        password:hashedpassword,
         study_level,
         course
     });
     newmentee.save();
 });
 
-app.post("/mentorregister",(req,res)=>{
+app.post("/mentorregister",async(req,res)=>{
     const {fname,lname,email,password,study_level,course,exp} = req.body;
     console.log(req.body);
+    const hashedpassword =  await bcrypt.hash(password,10);
     const newmentor = new Mentor({
         fname,
         lname,
         email,
-        password,
+        password:hashedpassword,
         study_level,
         course,
         exp
     });
     newmentor.save();
 });
+
+// login 
+
+app.post("/login",async(req,res)=>{
+  const {email,password} = req.body;
+  const user = await Mentee.findOne({email});
+  console.log(user);
+  if(!user){
+    res.status(501).render("login");
+  }
+  const isvalid = await bcrypt.compare(password,user.password);
+  console.log(isvalid);
+  if(!isvalid){
+    res.status(500).render("login")
+  }
+  res.status(201).render("index")
+});
+
+app.post("/mentorlogin",async(req,res)=>{
+    const {email,password} = req.body;
+    const user = await Mentor.findOne({email});
+    console.log(user);
+    if(!user){
+      res.status(501).render("login");
+    }
+    const isvalid = await bcrypt.compare(password,user.password);
+    console.log(isvalid);
+    if(!isvalid){
+      res.status(500).render("login")
+    }
+    res.status(201).render("index");
+})
 app.listen(PORT,()=>console.log(`Listening to port ${PORT}`));
--- public/login.js
+++ public/login.js
@@ -1,5 +1,6 @@
 const mentee =  document.getElementById('mentee')
 const mentor = document.getElementById('mentor')
+const form = document.querySelector(".form");
 let mn = true;
 let mt = false;
 mentee.addEventListener('click',()=>{
@@ -12,21 +13,23 @@ mentee.addEventListener('click',()=>{
         mentor.style.color = '#aaaaaa'
         mentor.style.borderBottom = 'none'
         mt = false
+        form.action = "/login"
     }
 
 })
 
 mentor.addEventListener('click',()=>{
     if(mt){
-
+                   
     }else{
         mentor.style.color = '#2d1b81'
         mentor.style.borderBottom = 'solid 2px #2d1b81'
         mt = true
-
         mentee.style.color = '#aaaaaa'
         mentee.style.borderBottom = 'none'
         mn = false
+        form.action = "/mentorlogin"
+
     }
 
 })
